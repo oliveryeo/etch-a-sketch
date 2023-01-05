76b9b861ca86a1a4deb540dfcc614d3e3f88cc9e
oliveryjy <yjyoliver@hotmail.com>
2023-01-05 15:20:33 +0800

    Added intentional coloring capability
    Previously, the mouse would hover and paint the canvas, which does not
    make sense. Hence, coloring will only occur if the user holds his mouse
    down on the canvas.

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
 // Create a script that inputs the desired grid dimensions (start with 16x16)
-
-// Initialise dimensions e.g. 16x16
-let gridRows = 16;
-let gridColumns = 16;
-
-// Make the grids
 const gridSquares = document.querySelector('div.grid-squares');
-makeGrid(gridRows, gridColumns);
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
 
-// Change color of grid upon hover to black
-const columns = document.querySelectorAll('.column');
-columns.forEach(changeColor);
+// Initialise initial appearance
+initialize();
 
 // Allow user input for dimension
 const inputButton = document.querySelector('.input');
-inputButton.addEventListener('click', () => {
-    dimension = parseInt(prompt("What dimensions would you like? (input a number up to 100)"));
+inputButton.addEventListener('click', changeInitialization);
 
-    // Check if input is > 100, and prompt again if so.
-    while (dimension > 100) {
-        dimension = parseInt(prompt("Please choose a number up to 100 only"));
-    }
-
-    gridRows = dimension;
-    gridColumns = dimension;
-
-    // Remove the grid by clearing the elements
-    // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
-    gridSquares.textContent = '';
-
-    // Add the grid again
-    makeGrid(gridRows, gridColumns);
-
-    // Have to select columns again because they don't exist after content removal.
-    const columns = document.querySelectorAll('.column');
-    columns.forEach(changeColor);
-})
 
 
 
@@ -61,7 +36,54 @@ function makeGrid(rows, columns) {
 
 // Change background-color upon hover on the element
 function changeColor(column) {
-    column.addEventListener('mouseover', () => {
+    // If the mouse is held down and hovered over the element → Change color.
+    column.addEventListener('mouseover', (e) => {
+        console.log(mouseDown);
+        // If mouse is not held down, do not color.
+        if (e.type === 'mouseover' && !mouseDown){
+            return;
+        }
         column.classList.add(`color-change`);
     })
+
+    // If the element is 'mousedown'ed, change color immediately.
+    column.addEventListener('mousedown', () => {
+        column.classList.add(`color-change`);
+    })
+}
+
+function initialize() {
+    // Initialise dimensions e.g. 16x16
+    let gridRows = 16;
+    let gridColumns = 16;
+
+    // Make the grids
+    makeGrid(gridRows, gridColumns);
+
+    // Change color of grid upon hover to black
+    const columns = document.querySelectorAll('.column');
+    columns.forEach(changeColor);
+}
+
+function changeInitialization() {
+    dimension = parseInt(prompt("What dimensions would you like? (input a number up to 100)"));
+
+    // Check if input is > 100, and prompt again if so.
+    while (dimension > 100) {
+        dimension = parseInt(prompt("Please choose a number up to 100 only"));
+    }
+
+    gridRows = dimension;
+    gridColumns = dimension;
+
+    // Remove the grid by clearing the elements
+    // https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    gridSquares.textContent = '';
+
+    // Add the grid again
+    makeGrid(gridRows, gridColumns);
+
+    // Have to select columns again because they don't exist after content removal.
+    const columns = document.querySelectorAll('.column');
+    columns.forEach(changeColor);
 }
